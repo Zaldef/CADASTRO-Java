394cdd4998d36127c3005b4f75900b845193f15f
Taynara Araújo <112909056+Thay483@users.noreply.github.com>
2023-10-12 21:04:35 -0300

    Criação da classe de produtos

--- ClasseProdutos.java
+++ ClasseProdutos.java
@@ -1,4 +1,37 @@
 public class ClasseProdutos {
+    private String descricao;
+    private int id;
+    private double valor;
+    private int quantidade;
     
+    public ClasseProdutos(String descricao, int valor){
+        this.descricao = descricao;
+        this.valor = valor;
+
+        public String getDescricao(){
+            return descricao;
+        }
+        public int getId(){
+
+        }
+        public double getValor(){
+
+        }
+        public int getQuantidade(){
+
+        }
+        public String setDescricao(String descricao){
+
+        }
+        public int setId(int id){
+
+        }
+        public double setValor(double valor){
+
+        }
+        public String toString(){
+            System.out.println("Valores dos Produtos...");
+        }
+    }
     
 }
